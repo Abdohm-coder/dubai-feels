a97cc8a3a7fbec2c6b45c4b55f133272a02f0c48
Abdohm-coder <hadjmedabdo@gmail.com>
2023-06-29 18:00:27 +0100

    test api

---
+++ pages/api/countries.js
@@ -0,0 +1,11 @@
+export default async function handler (_, res) {
+    try{
+        const response = await axios.get("https://www.raynab2b.com/api/Tour/countries");
+        const data = await response.json();
+        console.log(data)
+        res.status(200).json(data);
+    }catch(err) {
+        console.error(err);
+        res.status(500).json({ error: "Internal Server Error :(" })
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import Wrapper from "./layout/wrapper";
 import Home5 from "./home/home_5";
+import { useEffect } from "react";
+import axios from "axios";
 
 const MainRoot = () => {
 
   useEffect(() => {
-    
+    const fetchData = async() => {
+
+      const response = await axios.get("https://www.raynab2b.com/api/Tour/countries");
+      console.log("Data from Frontend: ", response)
+    }
+    fetchData();
+    const fetchApi = async () => {
+      const response = await axios.get("/api/countries");
+      console.log("Data from Nodejs", response)
+    } 
+    fetchApi();
   }, [])
   return (
     <Wrapper>
