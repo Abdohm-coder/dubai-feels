3ea2e259f0751f3a8f34628e77c9c15fbf238419
Abdohm-coder <hadjmedabdo@gmail.com>
2023-06-29 17:39:27 +0100

    upload the template

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Wrapper from "./layout/wrapper";
+import Home5 from "./home/home_5";
+
+const MainRoot = () => {
+
+  useEffect(() => {
+    
+  }, [])
+  return (
+    <Wrapper>
+      <Home5 />
+    </Wrapper>
+  );
+};
+
+export default MainRoot;
