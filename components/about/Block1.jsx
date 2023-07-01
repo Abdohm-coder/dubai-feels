14f446cad4ce2792a24dd2578937bd3c7a013a43
Abdohm-coder <hadjmedabdo@gmail.com>
2023-07-01 18:28:31 +0100

    fix about page

--- components/about/Block1.jsx
+++ components/about/Block1.jsx
@@ -3,32 +3,52 @@ import Image from "next/image";
 const Block1 = () => {
   return (
     <>
-      <div className="col-lg-5">
+      <div>
         <h2 className="text-30 fw-600">About GoTrip.com</h2>
         <p className="mt-5">These popular destinations have a lot to offer</p>
         <p className="text-dark-1 mt-60 lg:mt-40 md:mt-20">
-          London is a shining example of a metropolis at the highest peak of
-          modernity and boasts an economy and cultural diversity that’s the envy
-          of other global superpowers.
-          <br />
-          <br />
-          Take the opportunity to acquaint yourself with its fascinating history
-          chronicled by institutions like the British Museum as well as see how
-          far it has come by simply riding the Tube and passing by celebrated
-          landmarks like Buckingham Palace, Westminster Abbey, and marvels like
-          Big Ben, the London Eye, and the Tower Bridge.
+          You can consider Dubai feel like the gateway to the world of
+          entertainment in Dubai for the global tourists. Assume us as the Dubai
+          gateway for you! We are always there to provide you with the best
+          possible activities around the world. You will receive a special
+          travel experience. We are a specialist retailer to book your
+          attraction, travel, and experience tickets. We are working with a
+          great number of experiences (almost all the quality ones) in Dubai
+          suitable for all the global tourists. Operating with the best local
+          providers from all over the world in Dubai, we offer travel services,
+          products, and content, making sure that our clients are perfectly
+          informed and, in a position, to select the ideal experience. We only
+          present the best to all our clients at Dubai feel! Our expert travel
+          team handpicked products. Our group is always looking for quality,
+          unique travel experiences to suit according to different interests and
+          budgets clients. This comprehensive process of selection of the
+          activities, trips, and tours has led to an increasing client base of
+          over 150 nations worldwide. We are proud of our high client
+          satisfaction ratings of 90% (provided by an independent rating
+          agency). The Dubai feel is the tour company which only deals with
+          activities and experiences in Dubai only. We are the members of top
+          travel groups in the world and have a high reputation of providing
+          outstanding service in Dubai for all sorts of activities. We made a
+          portfolio website called Dubai feel through which we sell our
+          activities and tours to clients across the globe coming to spend a
+          meaningful holiday or for some business activities while taking some
+          time out to feel the Dubai entertainment. We are constantly searching
+          for interesting talent and exciting projects to add to our arsenal. We
+          have a well-organized team to cover every aspect of the business,
+          including proper support functions, product management, customer
+          support, marketing, and technology. Each team must be the finest in
+          its class to service the ideal model of business. For instance, the
+          team of technology could work on a model to allow cross-device
+          customers to travel through cloud technologies that offer both
+          security and opportunity to scale. The marketing group continues to
+          try to be present at the top position of the Google search results to
+          reach more potential clients. Customer support may help the clients
+          with their ideal advice and support to help them choose their next
+          ideal travel experience. At the same time, the team of product
+          management could use data to comprehend what we may deliver to our
+          market sections to keep on expanding and evolving. Our compact yet
+          efficient group of professionals ensures accountability and ownership.
         </p>
-      </div>
-      {/* End .col */}
-
-      <div className="col-lg-6">
-        <Image
-          width={400}
-          height={400}
-          src="/img/pages/about/2.png"
-          alt="image"
-          className="rounded-4 w-100"
-        />
       </div>
       {/* End .col */}
     </>
--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,8 @@
 import dynamic from "next/dynamic";
-import CallToActions from "../components/common/CallToActions";
 import Seo from "../components/common/Seo";
 import DefaultHeader from "../components/header/default-header";
 import DefaultFooter from "../components/footer/default";
-import WhyChoose from "../components/block/BlockGuide";
-import Block1 from "../components/about/Block1";
 import Image from "next/image";
-import Counter from "../components/counter/Counter";
-import Team1 from "../components/team/Team1";
-import Testimonial from "../components/testimonial/Testimonial";
-import Counter2 from "../components/counter/Counter2";
-import Brand from "../components/brand/Brand";
 
 const About = () => {
   return (
@@ -61,110 +53,64 @@ const About = () => {
                 <p className=" sectionTitle__text mt-5 sm:mt-0">
                   These popular destinations have a lot to offer
                 </p>
+                <p className="text-dark-1 mt-60 lg:mt-40 md:mt-20">
+                  You can consider Dubai feel like the gateway to the world of
+                  entertainment in Dubai for the global tourists. Assume us as
+                  the Dubai gateway for you! We are always there to provide you
+                  with the best possible activities around the world. You will
+                  receive a special travel experience. We are a specialist
+                  retailer to book your attraction, travel, and experience
+                  tickets. We are working with a great number of experiences
+                  (almost all the quality ones) in Dubai suitable for all the
+                  global tourists. Operating with the best local providers from
+                  all over the world in Dubai, we offer travel services,
+                  products, and content, making sure that our clients are
+                  perfectly informed and, in a position, to select the ideal
+                  experience. We only present the best to all our clients at
+                  Dubai feel! Our expert travel team handpicked products. Our
+                  group is always looking for quality, unique travel experiences
+                  to suit according to different interests and budgets clients.
+                  This comprehensive process of selection of the activities,
+                  trips, and tours has led to an increasing client base of over
+                  150 nations worldwide. We are proud of our high client
+                  satisfaction ratings of 90% (provided by an independent rating
+                  agency). The Dubai feel is the tour company which only deals
+                  with activities and experiences in Dubai only. We are the
+                  members of top travel groups in the world and have a high
+                  reputation of providing outstanding service in Dubai for all
+                  sorts of activities. We made a portfolio website called Dubai
+                  feel through which we sell our activities and tours to clients
+                  across the globe coming to spend a meaningful holiday or for
+                  some business activities while taking some time out to feel
+                  the Dubai entertainment. We are constantly searching for
+                  interesting talent and exciting projects to add to our
+                  arsenal. We have a well-organized team to cover every aspect
+                  of the business, including proper support functions, product
+                  management, customer support, marketing, and technology. Each
+                  team must be the finest in its class to service the ideal
+                  model of business. For instance, the team of technology could
+                  work on a model to allow cross-device customers to travel
+                  through cloud technologies that offer both security and
+                  opportunity to scale. The marketing group continues to try to
+                  be present at the top position of the Google search results to
+                  reach more potential clients. Customer support may help the
+                  clients with their ideal advice and support to help them
+                  choose their next ideal travel experience. At the same time,
+                  the team of product management could use data to comprehend
+                  what we may deliver to our market sections to keep on
+                  expanding and evolving. Our compact yet efficient group of
+                  professionals ensures accountability and ownership.
+                </p>
               </div>
             </div>
           </div>
           {/* End .row */}
 
-          <div className="row y-gap-40 justify-between pt-50">
-            <WhyChoose />
-          </div>
           {/* End .row */}
         </div>
         {/* End .container */}
       </section>
       {/* End Why Choose Us section */}
-
-      <section className="layout-pt-md">
-        <div className="container">
-          <div className="row y-gap-30 justify-between items-center">
-            <Block1 />
-          </div>
-        </div>
-      </section>
-      {/* End about block section */}
-
-      <section className="pt-60">
-        <div className="container">
-          <div className="border-bottom-light pb-40">
-            <div className="row y-gap-30 justify-center text-center">
-              <Counter />
-            </div>
-          </div>
-        </div>
-      </section>
-      {/* End counter Section */}
-
-      <section className="layout-pt-lg layout-pb-lg">
-        <div className="container">
-          <div className="row y-gap-20 justify-between items-end">
-            <div className="col-auto">
-              <div className="sectionTitle -md">
-                <h2 className="sectionTitle__title">Our Team</h2>
-                <p className=" sectionTitle__text mt-5 sm:mt-0">
-                  Lorem ipsum dolor sit amet
-                </p>
-              </div>
-            </div>
-          </div>
-          {/* End .row */}
-
-          <div className=" pt-40 js-section-slider">
-            <div className="item_gap-x30">
-              <Team1 />
-            </div>
-          </div>
-          {/* End  js-section-slider */}
-        </div>
-        {/* End container */}
-      </section>
-      {/* End team section */}
-
-      <section className="section-bg layout-pt-lg layout-pb-lg">
-        <div className="section-bg__item -mx-20 bg-light-2" />
-        <div className="container">
-          <div className="row justify-center text-center">
-            <div className="col-auto">
-              <div className="sectionTitle -md">
-                <h2 className="sectionTitle__title">
-                  Overheard from travelers
-                </h2>
-                <p className=" sectionTitle__text mt-5 sm:mt-0">
-                  These popular destinations have a lot to offer
-                </p>
-              </div>
-            </div>
-          </div>
-          {/* End .row */}
-
-          <div className="overflow-hidden pt-80 js-section-slider">
-            <div className="item_gap-x30">
-              <Testimonial />
-            </div>
-          </div>
-          {/* End .overflow-hidden */}
-
-          <div className="row y-gap-30 items-center pt-40 sm:pt-20">
-            <div className="col-xl-4">
-              <Counter2 />
-            </div>
-            {/* End .col */}
-
-            <div className="col-xl-8">
-              <div className="row y-gap-30 justify-between items-center">
-                <Brand />
-              </div>
-            </div>
-            {/* End .col */}
-          </div>
-          {/* End .row */}
-        </div>
-        {/* End .container */}
-      </section>
-      {/* End testimonial section */}
-
-      <CallToActions />
-      {/* End Call To Actions Section */}
 
       <DefaultFooter />
       {/* End Call To Actions Section */}
