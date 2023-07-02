1b5080918a78863caec43d03d67fbd2abf91879c
Abdohm-coder <hadjmedabdo@gmail.com>
2023-07-02 18:22:08 +0100

    feat search page

--- components/hero/hero-5/MainFilterSearchBox.jsx
+++ components/hero/hero-5/MainFilterSearchBox.jsx
@@ -31,7 +31,7 @@ const MainFilterSearchBox = () => {
           <div className="button-item">
             <button
               className="mainSearch__submit button -dark-1 py-15 px-35 h-60 col-12 rounded-4 bg-blue-1 text-white"
-              onClick={() => Router.push("/tour/tour-list-v1")}
+              onClick={() => Router.push("/search?keyword=")}
             >
               <i className="icon-search text-20 mr-10" />
               Search
---
+++ pages/search/index.js
@@ -0,0 +1,65 @@
+import Seo from "../../components/common/Seo";
+import Header11 from "../../components/header/header-11";
+import DefaultFooter from "../../components/footer/default";
+import TopHeaderFilter from "../../components/tour-list/tour-list-v1/TopHeaderFilter";
+import TourProperties from "../../components/tour-list/tour-list-v1/TourProperties";
+import { useSearchParams } from "next/navigation";
+import { capitalizeString } from "../../utils/capitalize-string";
+
+const Index = () => {
+  const searchParams = useSearchParams();
+  const keyword = searchParams.get("keyword");
+  const title = capitalizeString(keyword?.replaceAll("-", " "));
+  return (
+    <>
+      <Seo pageTitle={`Search Result For ${title}`} />
+      {/* End Page Title */}
+
+      <div className="header-margin"></div>
+      {/* header top margin */}
+
+      <Header11 />
+      {/* End Header 1 */}
+
+      <section className="pt-40 pb-40 bg-light-2">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="text-center">
+                <h1 className="text-30 fw-600">
+                  Search Result For &quot;{title}&quot;
+                </h1>
+              </div>
+              {/* End text-center */}
+            </div>
+            {/* End col-12 */}
+          </div>
+        </div>
+      </section>
+      {/* Top SearchBanner */}
+
+      <section className="layout-pt-md layout-pb-lg">
+        <div className="container">
+          <div className="row y-gap-30">
+            <div>
+              <TopHeaderFilter />
+              <div className="mt-30"></div>
+              {/* End mt--30 */}
+              <div className="row y-gap-30">
+                <TourProperties />
+              </div>
+            </div>
+            {/* End .col for right content */}
+          </div>
+          {/* End .row */}
+        </div>
+        {/* End .container */}
+      </section>
+      {/* End layout for listing sidebar and content */}
+
+      <DefaultFooter />
+    </>
+  );
+};
+
+export default Index;
