50295fbe844a13dc2ce0e3abe448140707e92a3a
Abdohm-coder <hadjmedabdo@gmail.com>
2023-07-09 19:31:58 +0100

    style navbar menu items like sidebar menu

--- src/components/header/CategoriesMegaMenu.tsx
+++ src/components/header/CategoriesMegaMenu.tsx
@@ -1,69 +1,77 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import { categorieMegaMenuItems } from "../../data/mainMenuData";
-import { isActiveLink } from "../../utils/linkActiveChecker";
+import { isActiveLink } from "@/utils/linkActiveChecker";
+import { categorieMobileItems } from "@/data/mainMenuData";
+import { Menu, MenuItem, ProSidebarProvider, SubMenu } from "react-pro-sidebar";
 
 const CategoriesMegaMenu = () => {
   const router = useRouter();
 
-  const itemList = [
-    "Flying Experiences",
-    "Desert Safari",
-    "City Tours",
-    "Cruises",
-    "Water Activities",
-    "Theme Parks & Water Parks",
-    "Limousine Tours",
-    "Luxury Yacht Tours",
-    { name: "Private Transfers", routePath: "/private-transfers" },
-    { name: "Adventure Tours", routePath: "/adventure-tours" },
-    { name: "Sightseeing", routePath: "/sightseeing" },
-  ];
+  // const itemList = [
+  //   "Flying Experiences",
+  //   "Desert Safari",
+  //   "City Tours",
+  //   "Cruises",
+  //   "Water Activities",
+  //   "Theme Parks & Water Parks",
+  //   "Limousine Tours",
+  //   "Luxury Yacht Tours",
+  //   { name: "Private Transfers", routePath: "/private-transfers" },
+  //   { name: "Adventure Tours", routePath: "/adventure-tours" },
+  //   { name: "Sightseeing", routePath: "/sightseeing" },
+  // ];
 
   return (
-    <Tabs className="tabs -underline-2 js-tabs">
-      <TabList className="tabs__controls row x-gap-40 y-gap-10 lg:x-gap-20 pb-30 js-tabs-controls">
-        {itemList.map((item, i) => (
-          <Tab className="col-auto" key={i}>
-            <button
-              onClick={() => {
-                if (typeof item !== "string")
-                  router.push(`/dubai-activities${item.routePath}`);
-              }}
-              className="tabs__button text-light-1 fw-500 js-tabs-button">
-              {typeof item === "string" ? item : item.name}
-            </button>
-          </Tab>
+    <ProSidebarProvider>
+      <Menu>
+        {categorieMobileItems.map((item) => (
+          <>
+            {item.routePath ? (
+              <MenuItem
+                key={`item-${item.title}`}
+                component={
+                  <Link
+                    href={`/dubai-activities${item.routePath}`}
+                    className={
+                      isActiveLink(
+                        `/dubai-activities${item.routePath}`,
+                        router.asPath
+                      )
+                        ? "menu-active-link"
+                        : ""
+                    }
+                  />
+                }>
+                {item.title}
+              </MenuItem>
+            ) : (
+              <SubMenu label={item.title} key={item.id}>
+                {item.menuItems?.map((menu) => (
+                  <MenuItem
+                    style={{ paddingLeft: "9px" }}
+                    key={`item-${menu.name}`}
+                    component={
+                      <Link
+                        href={`/dubai-activities${menu.routePath}`}
+                        className={
+                          isActiveLink(
+                            `/dubai-activities${menu.routePath}`,
+                            router.asPath
+                          )
+                            ? "menu-active-link"
+                            : ""
+                        }
+                      />
+                    }>
+                    {menu.name}
+                  </MenuItem>
+                ))}
+              </SubMenu>
+            )}
+          </>
         ))}
-      </TabList>
-      {/* End tab-controls */}
-
-      <div className="tabs__content js-tabs-content">
-        {categorieMegaMenuItems.map((megaMenu) => (
-          <TabPanel className={"row"} key={megaMenu.id}>
-            {megaMenu?.menuCol?.map((megaCol, i) => (
-              <div
-                key={i}
-                className={`col-4
-                  ${
-                    isActiveLink(
-                      `/dubai-activities${megaCol.routePath}`,
-                      router.asPath
-                    )
-                      ? "current"
-                      : ""
-                  }}`}>
-                <Link href={`/dubai-activities${megaCol.routePath}`}>
-                  {megaCol.name}
-                </Link>
-              </div>
-            ))}
-          </TabPanel>
-        ))}
-      </div>
-      {/* End tab_content */}
-    </Tabs>
+      </Menu>
+    </ProSidebarProvider>
   );
 };
 
